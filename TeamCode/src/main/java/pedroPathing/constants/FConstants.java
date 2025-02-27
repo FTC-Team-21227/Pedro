24f555bc6d4156ef1070472c84595505de4dc9c0
FTC-Team-21227 <teamlegendftc@gmail.com>
2025-02-26 21:51:07 -0800

    constants setup/tuning prereqs

--- TeamCode/src/main/java/pedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/FConstants.java
@@ -8,19 +8,19 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class FConstants {
     static {
-        FollowerConstants.localizers = Localizers.THREE_WHEEL;
+        FollowerConstants.localizers = Localizers.PINPOINT;
 
-        FollowerConstants.leftFrontMotorName = "leftFront";
-        FollowerConstants.leftRearMotorName = "leftRear";
-        FollowerConstants.rightFrontMotorName = "rightFront";
-        FollowerConstants.rightRearMotorName = "rightRear";
+        FollowerConstants.leftFrontMotorName = "W_FL";
+        FollowerConstants.leftRearMotorName = "W_BL";
+        FollowerConstants.rightFrontMotorName = "W_FR";
+        FollowerConstants.rightRearMotorName = "W_BR";
 
         FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
         FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
         FollowerConstants.rightFrontMotorDirection = DcMotorSimple.Direction.FORWARD;
         FollowerConstants.rightRearMotorDirection = DcMotorSimple.Direction.FORWARD;
 
-        FollowerConstants.mass = 13;
+        FollowerConstants.mass = 36; //kg
 
         FollowerConstants.xMovement = 57.8741;
         FollowerConstants.yMovement = 52.295;
